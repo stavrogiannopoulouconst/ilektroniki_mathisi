3e8a85c1f45832a91293d6967ddf3d711b51756b
Constantina <constantina.st4@gmail.com>
2026-05-17 21:07:51 +0300

    Added messages

---
+++ Assets/Scripts/StairsMessage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DoorMessage : MonoBehaviour
+{
+    public GameObject message;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Touched");
+
+        if (other.CompareTag("Player"))
+        {
+            message.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            message.SetActive(false);
+        }
+    }
+}
