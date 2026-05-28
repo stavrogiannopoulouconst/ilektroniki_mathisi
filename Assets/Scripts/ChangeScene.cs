665a6de5154eab32f96816924ac890a3f5acec67
Constantina <constantina.st4@gmail.com>
2026-05-28 13:07:55 +0300

    fixed indoors scene

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,11 +5,29 @@ public class Door : MonoBehaviour
 {
     public string sceneName;
 
+    private bool playerNear = false;
+
+    void Update()
+    {
+        if (playerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            playerNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
         }
     }
 }
